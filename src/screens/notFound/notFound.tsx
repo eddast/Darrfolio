0f63455424c67dd626027929a4e417ca80d76ded
Edda <eddasteinunn@hotmail.com>
2022-03-18 20:17:14 +0000

    lol

--- src/screens/notFound/notFound.tsx
+++ src/screens/notFound/notFound.tsx
@@ -1,5 +1,11 @@
-import { Text } from 'ui/core'
+import { Box, Text } from 'ui/core'
 
-const NotFound = () => <Text variant="h1">404 Page</Text>
+const NotFound = () => (
+  <Box width="full" height="viewportHeight">
+    <Text variant="h1">404 Page</Text>
+    <Text>Are you wondering why?!</Text>
+    <Text fontWeight="bold">READ THE README</Text>
+  </Box>
+)
 
 export default NotFound
